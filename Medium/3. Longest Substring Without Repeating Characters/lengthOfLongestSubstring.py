e5bd6f1b972da8f32265e3254d4c41f9f8361e9f
spencermcnamara <smcn0012@student.monash.edu>
2024-01-24 20:52:35 +1100

    Changed to check if the the substring finishes at the end of the string

--- Medium/3. Longest Substring Without Repeating Characters/lengthOfLongestSubstring.py
+++ Medium/3. Longest Substring Without Repeating Characters/lengthOfLongestSubstring.py
@@ -6,7 +6,7 @@ class Solution(object):
         """
         last_instance = [-1 for _ in range(ord('~') - ord(' '))]
         substring_start = 0
-        longest_substring = -1
+        longest_substring = 0
         for s_index, char in enumerate(s):
             char_index = ord(char) - ord(' ')
             if last_instance[char_index] != -1:
@@ -15,8 +15,8 @@ class Solution(object):
                 substring_start = last_instance[char_index] + 1
                 last_instance[char_index] = s_index
             last_instance[char_index] = s_index
-        if longest_substring == -1:
-            longest_substring = len(s)
+        if len(s) - substring_start > longest_substring:
+            longest_substring = len(s) - substring_start
 
         return longest_substring
 
