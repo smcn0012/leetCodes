e27d2933c7b92d958d7f7cf268d03b540294c3bf
spencermcnamara <smcn0012@student.monash.edu>
2024-01-24 21:10:05 +1100

    changed to only look at characters since the last repeat

--- Medium/3. Longest Substring Without Repeating Characters/lengthOfLongestSubstring.py
+++ Medium/3. Longest Substring Without Repeating Characters/lengthOfLongestSubstring.py
@@ -4,21 +4,17 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        last_instance = [-1 for _ in range(ord('~') - ord(' '))]
+        last_instance = [-1 for _ in range(ord('~') - ord(' ') + 1)]
         substring_start = 0
         longest_substring = 0
         for s_index, char in enumerate(s):
             char_index = ord(char) - ord(' ')
-            if last_instance[char_index] != -1:
+            if last_instance[char_index] >= substring_start:
                 if s_index - substring_start > longest_substring:
                     longest_substring = s_index - substring_start
                 substring_start = last_instance[char_index] + 1
-                last_instance[char_index] = s_index
             last_instance[char_index] = s_index
         if len(s) - substring_start > longest_substring:
             longest_substring = len(s) - substring_start
 
         return longest_substring
-
-
-Solution().lengthOfLongestSubstring(' ')
