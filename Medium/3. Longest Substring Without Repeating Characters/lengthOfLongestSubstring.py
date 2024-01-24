56ae06b4feed288fa79dc1b85d60304b8740092e
spencermcnamara <smcn0012@student.monash.edu>
2024-01-24 20:38:40 +1100

    First atempt at 3. Longest Substring Without Repeating Characters

---
+++ Medium/3. Longest Substring Without Repeating Characters/lengthOfLongestSubstring.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        last_instance = [-1 for _ in range(ord('~') - ord(' '))]
+        substring_start = 0
+        longest_substring = 0
+        for s_index, char in enumerate(s):
+            char_index = ord(char) - ord(' ')
+            if last_instance[char_index] != -1:
+                if s_index - substring_start > longest_substring:
+                    longest_substring = s_index - substring_start
+                substring_start = last_instance[char_index] + 1
+                last_instance[char_index] = s_index
+            last_instance[char_index] = s_index
+        
+        return longest_substring
+
+
