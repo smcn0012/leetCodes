c27ddc050dcd76f29d5267fcfa7f209e04088293
spencermcnamara <smcn0012@student.monash.edu>
2024-03-06 21:21:41 +1100

    Redesigned my attempt

--- Medium/11. Container With Most Water/maxArea.py
+++ Medium/11. Container With Most Water/maxArea.py
@@ -1,16 +1,24 @@
 class Solution:
-    def maxArea(self, height: list[int]) -> int:
-        current_max = 0
-        for start, h1 in enumerate(height):
-            if start == len(height) - 1:
-                break
-            for end, h2 in enumerate(height[-1:start:-1]):
-                if h1 * (len(height) - end - start) <= current_max:
-                    break
-                if min(h1, h2) * (len(height) - end - start - 1) > current_max:
-                    current_max = min(h1, h2) * (len(height) - end - start - 1)
+    def maxArea(self, heights: list[int]) -> int:
+        MAX_HEIGHT = max(heights)
 
-        return current_max
+        max_area = 0
+        start = 0
+        end = len(heights) - 1
+        target_height = 0
+        while target_height <= MAX_HEIGHT and start < end:
+            target_height = max_area / (end - start)
+            start_height = heights[start]
+            end_height = heights[end]
+            if start_height > target_height and end_height > target_height:
+                max_area = min(start_height, end_height) * (end - start)
+            else:
+                if start_height < end_height:
+                    start += 1
+                else:
+                    end -= 1
+
+        return max_area
 
 print(Solution().maxArea([1,8,6,2,5,4,8,3,7]))
 
